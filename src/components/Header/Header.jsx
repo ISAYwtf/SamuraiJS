0553f6312b2149b0f1dc26925cc420e055080061
Iskander <iskander.aydynov@mail.ru>
2021-01-27 20:22:47 +0300

    stage-61 - Login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import classes from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = ({isAuth, login}) => {
     return (
         <header className={classes.header}>
             <img src="https://www.freelogodesign.org/Content/img/logo-ex-7.png" alt="logo" />
+            <div className={classes.login}>
+                {isAuth ? login
+                : <NavLink to={`/login`}>Login</NavLink>}
+            </div>
         </header>
     )
 }
